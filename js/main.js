6d3b2ac366eeee6ee0b6dbe5f723128342d8f4ac
anouther <155570107+anouther@users.noreply.github.com>
2025-05-18 08:27:50 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Happy Valentine').split('');
+        const titles = ('The One I’m Lucky to Love').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "i don't know how to thank u, but i'm so glad u came into my life, cuz u made it so much better";
+            subtitle.innerText = "Hi kakak sayang. Thank you for being so kind, thoughtful, and always here for me. I feel so lucky to love you and to be loved by you. You’re my favorite comfort, my calm, and my home. I love you wholeheartedly 🤍 -Eiren";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
